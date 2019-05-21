f8da1a7dddc9f2603d92c403aea22f254163f092
szymonqqaz <szymonqqaz22@gmail.com>
2019-05-21 12:58:21 +0200

    Add desciption to SEO

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -5,6 +5,10 @@ const SEO = () => (
   <Helmet>
     <html lang="pl" />
     <title>minecraft page</title>
+    <meta
+      name="description"
+      content="Serwer micecraft z giliami i innymi ciekawymi rzeczami o nazwie NBWLM"
+    />
   </Helmet>
 );
 
